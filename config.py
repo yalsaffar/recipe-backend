3ae5c55eb8dc8854318cd54142bdfa6b283ba607
yalsaffar <95584811+yalsaffar@users.noreply.github.com>
2022-12-17 20:26:13 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,29 @@
 from dotenv import load_dotenv
 import os
 
-
 load_dotenv()
 
-
 class Config(object):
-    SECRET_KEY = '??????'
+    SECRET_KEY = '???????'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-
-
-
-
-class DevelopmentConfig(Config):
-    print("mmmmmmmmmmmmmmmmmmkkkmmm")
-    print(os.getenv('DBUSER'))
+class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
-        dbuser=os.getenv('DBUSER'),
-        dbpass=os.getenv('DBPASS'),
-        dbhost=os.getenv('DBHOST'),
-        dbname=os.getenv('DBNAME')
+    dbuser=os.getenv('DBUSER'),
+    dbpass=os.getenv('DBPASS'),
+    dbhost=os.getenv('DBHOST'),
+    dbname=os.getenv('DBNAME')
     )
 
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
+    dbuser=os.getenv('DBUSER'),
+    dbpass=os.getenv('DBPASS'),
+    dbhost=os.getenv('DBHOST'),
+    dbname=os.getenv('DBNAME')
+    )
+    DEBUG = True
 
 class GithubCIConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///sqlitefile.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
     DEBUG = True
