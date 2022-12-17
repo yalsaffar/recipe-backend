231830a9261378512bfa952ce95f00193506d5bc
yalsaffar <95584811+yalsaffar@users.noreply.github.com>
2022-12-17 23:10:23 +0100

    Update config.py

--- config.py
+++ config.py
@@ -25,5 +25,5 @@ class DevelopmentConfig(Config):
     DEBUG = True
 
 class GithubCIConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///sqlitefile.db'
     DEBUG = True
